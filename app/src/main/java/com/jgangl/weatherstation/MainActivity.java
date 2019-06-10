effd07b50a3d6c08c4126ad699f210135d705060
Jgangl <joshua.gangl@gmail.com>
2019-06-10 10:45:33 -0400

    Added notifications for high/low temperatures.

--- app/src/main/java/com/jgangl/weatherstation/MainActivity.java
+++ app/src/main/java/com/jgangl/weatherstation/MainActivity.java
@@ -1,8 +1,19 @@
 package com.jgangl.weatherstation;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
@@ -31,6 +42,9 @@ public class MainActivity extends AppCompatActivity {
     private double currentHum;
     private double currentPress;
 
+    private double highTempThreshold;
+    private double lowTempThreshold;
+
     DecimalFormat precision = new DecimalFormat("0.0");
 
     @Override
@@ -52,6 +66,13 @@ public class MainActivity extends AppCompatActivity {
                     //Temperature comes as a 4 digit # ("4650" = 46.50 degrees Celsius)
                     currentTemp = celsiusToFahrenheit(dataToCelsius(Integer.parseInt(dataSnapshot.getValue().toString())));
                     tempTextView.setText(precision.format(currentTemp));
+
+                    if(currentTemp >= highTempThreshold){
+                        sendNotification();
+                    }
+                    else if(currentTemp <= lowTempThreshold){
+                        sendNotification();
+                    }
                 }
             }
 
@@ -61,7 +82,6 @@ public class MainActivity extends AppCompatActivity {
                 Log.w("Firebase Error", "loadPost:onCancelled", databaseError.toException());
             }
         };
-
         ValueEventListener humidityListener = new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
@@ -100,6 +120,10 @@ public class MainActivity extends AppCompatActivity {
         mDatabase.child("Temperature").addValueEventListener(temperatureListener);
         mDatabase.child("Humidity").addValueEventListener(humidityListener);
         mDatabase.child("Pressure").addValueEventListener(pressureListener);
+
+        createNotificationChannel();
+
+        sendNotification();
     }
 
     @Override
@@ -122,6 +146,61 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+
+    private static final int NOTIFICATION_ID = 0;
+    private static final String PRIMARY_CHANNEL_ID = "primary_notification_channel";
+    private static final String ACTION_UPDATE_NOTIFICATION =
+            "com.jgangl.notifyme.ACTION_UPDATE_NOTIFICATION";
+
+    private NotificationManager mNotifyManager;
+
+    public void createNotificationChannel() {
+        mNotifyManager = (NotificationManager)
+                getSystemService(NOTIFICATION_SERVICE);
+
+        if (android.os.Build.VERSION.SDK_INT >=
+                android.os.Build.VERSION_CODES.O) {
+            NotificationChannel notificationChannel = new NotificationChannel(PRIMARY_CHANNEL_ID,
+                    "Mascot Notification", NotificationManager
+                    .IMPORTANCE_HIGH);
+            notificationChannel.enableLights(true);
+            notificationChannel.setLightColor(Color.RED);
+            notificationChannel.enableVibration(true);
+            notificationChannel.setDescription("Notification from Mascot");
+
+            mNotifyManager.createNotificationChannel(notificationChannel);
+        }
+    }
+
+    public void sendNotification() {
+        //Intent updateIntent = new Intent(ACTION_UPDATE_NOTIFICATION);
+        //PendingIntent updatePendingIntent = PendingIntent.getBroadcast
+        //        (this, NOTIFICATION_ID, updateIntent, PendingIntent.FLAG_ONE_SHOT);
+
+        NotificationCompat.Builder notifyBuilder = getNotificationBuilder();
+
+        //notifyBuilder.addAction(R.drawable.ic_update, "Update Notification", updatePendingIntent);
+
+        mNotifyManager.notify(NOTIFICATION_ID, notifyBuilder.build());
+
+    }
+
+    private NotificationCompat.Builder getNotificationBuilder(){
+        Intent notificationIntent = new Intent(this, MainActivity.class);
+        PendingIntent notificationPendingIntent = PendingIntent.getActivity(this,
+                NOTIFICATION_ID, notificationIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+
+        NotificationCompat.Builder notifyBuilder = new NotificationCompat.Builder(this, PRIMARY_CHANNEL_ID)
+                .setContentTitle("Temperature Alert")
+                .setContentText("Temperature High")
+                .setContentIntent(notificationPendingIntent)
+                .setAutoCancel(true)
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setDefaults(NotificationCompat.DEFAULT_ALL);
+        return notifyBuilder;
     }
 
     //Convert Celsius to Fahrenheit
